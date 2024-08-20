1c4773894395b635b4c6e5001eec523272ce77ee
Phil Wang <lucidrains@gmail.com>
2024-08-20 08:36:46 -0700

    move the reflow into another file, as it may be obsoleted by the consistency fm paper

--- rectified_flow_pytorch/__init__.py
+++ rectified_flow_pytorch/__init__.py
@@ -1,8 +1,11 @@
 from rectified_flow_pytorch.rectified_flow import (
     RectifiedFlow,
-    Reflow,
     ImageDataset,
     Unet,
     Trainer,
+)
+
+from rectified_flow_pytorch.reflow import (
+    Reflow,
     ReflowTrainer
 )
